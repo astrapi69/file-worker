cee7212154a942a8e55bd0f3cb74144585897245
Asterios Raptis <asterios.raptis@gmx.net>
2017-05-27 18:00:47 +0200

    duplicate method renamed

--- src/main/java/de/alpharogroup/file/checksum/ChecksumExtensions.java
+++ src/main/java/de/alpharogroup/file/checksum/ChecksumExtensions.java
@@ -53,29 +53,29 @@ public final class ChecksumExtensions
 
 	/** The LOGGER. */
 	protected static final Logger LOGGER = Logger.getLogger(ChecksumExtensions.class.getName());
-	
+
 	/** The constant HEXADECIMAL_CHARACTER_CLASS. */
 	private static final String HEXADECIMAL_CHARACTER_CLASS = "[a-fA-F0-9]";
-	
+
 	/** The constant REGEX_VALIDATION_SHA1. */
 	public static final String REGEX_VALIDATION_SHA1 = HEXADECIMAL_CHARACTER_CLASS+"{40}";
-	
+
 	/** The constant REGEX_VALIDATION_MD5. */
 	public static final String REGEX_VALIDATION_MD5 = HEXADECIMAL_CHARACTER_CLASS+"{32}";
 	/** The constant REGEX_VALIDATION_SHA512. */
 	public static final String REGEX_VALIDATION_SHA512 = HEXADECIMAL_CHARACTER_CLASS + "{128}";
 
 	/**
-	 * Checks if the given value matches a SHA1 value pattern.
+	 * Checks if the given value matches a SHA512 value pattern.
 	 *
 	 * @param value
 	 *            the value
 	 * @return true, the given value matches a SHA1 value otherwise false
 	 */
-	public static boolean matchesSHA1(String value) {
+	public static boolean matchesSHA512(String value) {
 		return value.matches(REGEX_VALIDATION_SHA512);
 	}
-	
+
 	/**
 	 * Checks if the given value matches a SHA1 value pattern.
 	 *
@@ -85,7 +85,7 @@ public final class ChecksumExtensions
 	public static boolean matchesSHA1(String value) {
 	    return value.matches(REGEX_VALIDATION_SHA1);
 	}
-	
+
 	/**
 	 * Checks if the given value matches a MD5 value pattern.
 	 *
