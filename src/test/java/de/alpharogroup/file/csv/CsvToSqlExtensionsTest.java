c9e50665c2c3c5b82aa9bf57563097c5af2ef9eb
Asterios Raptis <asterios.raptis@gmx.net>
2018-06-15 15:30:56 +0200

    unit tests extended

---
+++ src/test/java/de/alpharogroup/file/csv/CsvToSqlExtensionsTest.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package de.alpharogroup.file.csv;
+
+import org.junit.Test;
+
+/**
+ * @author astrapi69
+ *
+ */
+public class CsvToSqlExtensionsTest
+{
+
+	/**
+	 * Test method for {@link de.alpharogroup.file.csv.CsvToSqlExtensions#extractSqlColumns(java.lang.String[])}.
+	 */
+	@Test
+	public final void testExtractSqlColumns() throws Exception
+	{
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.alpharogroup.file.csv.CsvToSqlExtensions#getCsvFileAsSqlInsertScript(java.lang.String, de.alpharogroup.file.csv.CsvBean)}.
+	 */
+	@Test
+	public final void testGetCsvFileAsSqlInsertScriptStringCsvBean() throws Exception
+	{
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.alpharogroup.file.csv.CsvToSqlExtensions#getCsvFileAsSqlInsertScript(java.lang.String, de.alpharogroup.file.csv.CsvBean, boolean, boolean)}.
+	 */
+	@Test
+	public final void testGetCsvFileAsSqlInsertScriptStringCsvBeanBooleanBoolean() throws Exception
+	{
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.alpharogroup.file.csv.CsvToSqlExtensions#getDataFromLine(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public final void testGetDataFromLine() throws Exception
+	{
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link de.alpharogroup.file.csv.CsvToSqlExtensions#getSqlData(java.lang.String[], java.lang.String[], java.lang.String[], java.util.Map, java.util.List, boolean)}.
+	 */
+	@Test
+	public final void testGetSqlData() throws Exception
+	{
+		// TODO
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
