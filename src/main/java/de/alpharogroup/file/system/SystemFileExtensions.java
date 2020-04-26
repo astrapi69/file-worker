fad80c291c1a9fd70fac0b7d2c3d9e587688b6f4
astrapi69 <asterios.raptis@gmx.net>
2020-04-26 14:45:48 +0200

    formatted, javadoc, sort members and organize imports

--- src/main/java/de/alpharogroup/file/system/SystemFileExtensions.java
+++ src/main/java/de/alpharogroup/file/system/SystemFileExtensions.java
@@ -24,9 +24,6 @@
  */
 package de.alpharogroup.file.system;
 
-import de.alpharogroup.file.compare.CompareFileExtensions;
-import de.alpharogroup.file.create.FileFactory;
-
 import java.io.File;
 
 /**
@@ -35,48 +32,56 @@ import java.io.File;
  * @version 1.0
  * @author Asterios Raptis
  */
-public final class SystemFileExtensions {
-    private SystemFileExtensions(){}
+public final class SystemFileExtensions
+{
+	/**
+	 * Gets the installation directory for Java Runtime Environment (JRE) from the system as
+	 * {@link File} object
+	 *
+	 * @return the installation directory for Java Runtime Environment (JRE) from the system as
+	 *         {@link File} object
+	 */
+	public static File getJavaHomeDir()
+	{
+		String userHomePath = System.getProperty("java.home");
+		return new File(userHomePath);
+	}
 
-    /**
-     * Gets the user home directory from the system as {@link File} object
-     *
-     * @return the user home directory from the system as {@link File} object
-     */
-    public static File getUserHomeDir(){
-        String userHomePath = System.getProperty("user.home");
-        return new File(userHomePath);
-    }
+	/**
+	 * Gets the temporary directory from the system as File object.
+	 *
+	 * @return the temporary directory from the system.
+	 */
+	public static File getTempDir()
+	{
+		String tmpdirPath = System.getProperty("java.io.tmpdir");
+		return new File(tmpdirPath);
+	}
 
-    /**
-     * Gets the user working directory from the system as {@link File} object
-     *
-     * @return the user working directory from the system as {@link File} object
-     */
-    public static File getUserWorkingDir(){
-        String userWorkingPath = System.getProperty("user.dir");
-        return new File(userWorkingPath);
-    }
+	/**
+	 * Gets the user home directory from the system as {@link File} object
+	 *
+	 * @return the user home directory from the system as {@link File} object
+	 */
+	public static File getUserHomeDir()
+	{
+		String userHomePath = System.getProperty("user.home");
+		return new File(userHomePath);
+	}
 
-    /**
-     * Gets the installation directory for Java Runtime Environment (JRE) from the system as {@link File} object
-     *
-     * @return the installation directory for Java Runtime Environment (JRE)  from the system as {@link File} object
-     */
-    public static File getJavaHomeDir(){
-        String userHomePath = System.getProperty("java.home");
-        return new File(userHomePath);
-    }
+	/**
+	 * Gets the user working directory from the system as {@link File} object
+	 *
+	 * @return the user working directory from the system as {@link File} object
+	 */
+	public static File getUserWorkingDir()
+	{
+		String userWorkingPath = System.getProperty("user.dir");
+		return new File(userWorkingPath);
+	}
 
-    /**
-     * Gets the temporary directory from the system as File object.
-     *
-     * @return the temporary directory from the system.
-     */
-    public static File getTempDir()
-    {
-        String tmpdirPath = System.getProperty("java.io.tmpdir");
-        return new File(tmpdirPath);
-    }
+	private SystemFileExtensions()
+	{
+	}
 
 }
