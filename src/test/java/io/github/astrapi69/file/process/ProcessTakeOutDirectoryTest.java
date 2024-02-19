f40ff29ec14c8c8f2f622b8b3a841e7d011e1a35
Asterios Raptis <asteri.raptis@gmail.com>
2024-02-19 16:22:04 +0100

    process of all takeout files as unit test

--- src/test/java/io/github/astrapi69/file/process/ProcessTakeOutDirectoryTest.java
+++ src/test/java/io/github/astrapi69/file/process/ProcessTakeOutDirectoryTest.java
@@ -4,6 +4,7 @@ import io.github.astrapi69.file.create.DirectoryFactory;
 import io.github.astrapi69.file.create.FileCreationState;
 import io.github.astrapi69.file.search.FileSearchExtensions;
 import io.github.astrapi69.file.system.SystemFileExtensions;
+import io.github.astrapi69.io.file.FileExtension;
 import io.github.astrapi69.zip4j.Zip4jExtensions;
 import net.lingala.zip4j.exception.ZipException;
 import org.testng.annotations.Test;
@@ -18,26 +19,49 @@ public class ProcessTakeOutDirectoryTest
 
 	/**
 	 * Test method for process the 'takeout-*' folders from google
+	 * ## Process flow
+	 *
+	 * 1. create temp folder for the output from the downloaded files if not exists create one.
+	 * 2. Download zip file from google drive
+	 * 2. run unit test for process
+	 * 3. in the unit test extract the zip file in the temp folder
+	 * 4. Remove all duplicated files and google related configuration files
+	 * 5. pack all files again in zip files
+	 * 6. upload all zip files to google drive
+	 *
+	 * # course of action
 	 */
-	@Test(enabled = false)
+	@Test(
+//			enabled = false
+	)
 	public void testProcess() throws ZipException {
 		File takeOutDir;
 		File outputDir;
 		List<File> actual;
 		String fileSearchPattern;
+		String zipFileNameWithExtension;
+		String zipFileName;
+		File userDownloadsDir;
+		File zipFile;
+		String zipExtension;
 
+		userDownloadsDir = SystemFileExtensions.getUserDownloadsDir();
 		if(!SystemFileExtensions.getUserTempDir().exists()) {
 			FileCreationState fileCreationState = DirectoryFactory.newDirectory(SystemFileExtensions.getUserTempDir());
 			System.out.println(fileCreationState);
 		}
 		outputDir = new File(SystemFileExtensions.getUserTempDir(), "output");
-		FileCreationState fileCreationState = DirectoryFactory.newDirectory(outputDir);
-		System.out.println(fileCreationState);
-//		takeOutDir = new File(SystemFileExtensions.getUserDownloadsDir(), "takeout-20230618T035040Z-201");
-//		actual = FileSearchExtensions.findAllFiles(takeOutDir, ".*json");
-//		System.out.println(actual);
+		if(!outputDir.exists()) {
+			FileCreationState fileCreationState = DirectoryFactory.newDirectory(outputDir);
+			System.out.println(fileCreationState);
+		}
 
-		final File zipFile = new File(SystemFileExtensions.getUserDownloadsDir(), "takeout-20230618T035040Z-097.zip");
+		zipExtension = FileExtension.ZIP.getExtension();
+		zipFileName = "takeout-20230616T214913Z-016";
+		zipFileNameWithExtension = zipFileName + zipExtension;
+		System.err.println("extracting "+ zipFileNameWithExtension);
+
+		zipFile = new File(userDownloadsDir, zipFileNameWithExtension);
 
 		final ZipFile zip4jZipFile = new ZipFile(zipFile);
 
