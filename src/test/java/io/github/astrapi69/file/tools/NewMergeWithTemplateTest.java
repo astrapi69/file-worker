b234139b52fe1a15c146d9bd2a5c4f168c87274b
Asterios Raptis <asteri.raptis@gmail.com>
2026-03-11 19:26:51 +0100

    fix: new internal unit test method created. formatted for build fix.

--- src/test/java/io/github/astrapi69/file/tools/NewMergeWithTemplateTest.java
+++ src/test/java/io/github/astrapi69/file/tools/NewMergeWithTemplateTest.java
@@ -84,7 +84,7 @@ class NewMergeWithTemplateTest
 	}
 
 	@Test
-//	@Disabled("only for internal and local use")
+	@Disabled("only for internal and local use")
 	void usesRealJsonDefaultsRueckkehrOderBefreiung() throws Exception
 	{
 		String bookProjectBaseDir = ProjectPaths.getDriveBookProjectBaseDir();
